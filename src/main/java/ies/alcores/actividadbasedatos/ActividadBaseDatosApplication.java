ca414cce689c261c9ab7b7c62719266f2c042ccf
paraelvideo06 <paraelvideo06@gmail.com>
2026-03-02 09:01:46 +0100

    9:01

---
+++ src/main/java/ies/alcores/actividadbasedatos/ActividadBaseDatosApplication.java
@@ -0,0 +1,13 @@
+package ies.alcores.actividadbasedatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadBaseDatosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActividadBaseDatosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ies/alcores/actividadbasedatos/ActividadBaseDatosApplicationTests.java
@@ -0,0 +1,13 @@
+package ies.alcores.actividadbasedatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadBaseDatosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
